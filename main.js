6f9ecab3f9414de318d60edb5c68ad748fe82d28
silentn9990 <linhtue191@gmail.com>
2025-10-20 03:49:21 +0000

    vsde

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ onload = () => {
             var letterLines = [
              "Hôm Nay Là Ngày 20/10 Là Ngày Phụ Nữ Việt Nam!",
             "Chúng Tớ Xin Chúc Các Bạn Nữ YK25C Mau Ăn Chóng Lớn Và Ngoan Ngoãn.Chúc Các Bạn Luôn Luôn Hạnh Phúc Và Luôn Nở Nụ Cười Trên Môi Nhé Và Đặc Biệt Là Không Được Suy Nghĩ Gì Tiêu Cực Nhé ",
-            "Đặc Biệt Là Chúc Các Bạn Nữ Ngày Càng Trở Nên Xinh Đẹp Hơn,Gặp Thật Nhiều May Mắn Trong Cuộc Sống Nhé "
+            "Đặc Biệt Là Chúc Các Bạn Nữ Ngày Càng Trở Nên Xinh Đẹp Hơn,Gặp Thật Nhiều May Mắn Trong Cuộc Sống Nhé ",
             "Và Đặc Biệt Là Chúc Các Bạn Sớm Đạt Ước Mơ Của Mình , Có Tất Cả Nhưng TRừ Vất Vả Ra Nhéeeeee"
         ];
     
